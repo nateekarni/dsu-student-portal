f2d65f6fe1d038cffd97a85b4839d62020ec3e6d
Nateekarn <nateekarn.ns@gmail.com>
2025-12-04 14:11:20 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -29,5 +29,7 @@ export const API = {
     // Admin Only
     getApplicants: () => fetchAPI('getApplicants'), // ต้องไปเพิ่มใน GAS
     updateDocStatus: (data) => fetchAPI('updateDocStatus', {}, 'POST', data),
-    adminLogin: (username, password) => fetchAPI('adminLogin', {}, 'POST', { username, password })
+    adminLogin: (username, password) => {
+        return fetchAPI('adminLogin', {}, 'POST', { username, password })
+    }
 };
